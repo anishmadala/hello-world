d0c5c5fac906cf12e96273780fd48c69a8dd0c7e
anishmadala <54461420+anishmadala@users.noreply.github.com>
2019-08-23 22:25:52 -0400

    I added the correct code

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/****************
+*Hello
+*****************
+*Lab 1 - I am showing this to my TA to show my command prompt is functional
+*Anish Madala
+*August 23, 2019
+*CMSC 255 Unknown section (Joined late)
+******************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Anish Madala");
+		System.out.println("Lab 1");
+		System.out.println("I am showing this to my TA to show my command prompt is functional");
+		System.out.println("August 23, 2019");
+		System.out.println("CMSC 255 Unknown section (Joined late)");
+		System.out.println();
+	}
+
+	public static void main (String[] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
